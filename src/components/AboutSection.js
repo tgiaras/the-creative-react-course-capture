3dbf4efc81439ab1e06ecd5a7d92df51126e10c1
Themis Giaras <tgiaras@hotmail.com>
2021-01-25 15:50:48 +1100

    2. About Section

---
+++ src/components/AboutSection.js
@@ -0,0 +1,28 @@
+const AboutSection = () => {
+  return (
+    <div>
+      <div className="description">
+        <div className="title">
+          <div className="hide">
+            <h2>We work to make</h2>
+          </div>
+          <div className="hide">
+            <h2>
+              your <span>dreams</span> come
+            </h2>
+          </div>
+          <div className="hide">
+            <h2>true</h2>
+          </div>
+        </div>
+        <p>
+          Contact us for any photography or videography ideas that you have. We
+          have professionals with amazing skills
+        </p>
+        <button>Contact us</button>
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
